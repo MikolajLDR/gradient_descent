78799141f5614f5e96e7bc347bb0ff314dd59f7f
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2021-10-21 12:21:06 +0200

    completed code

--- gradient_descent.py
+++ gradient_descent.py
@@ -8,12 +8,15 @@ def trunkate(number: float) -> float:
     '''
     return int(number*100)/100
 
+
 def find_minimum(function_gradient, cur_x, rate) -> list[float]:
     print(f'Starting at\nx = {cur_x:.2f}\n')
-    iterations = 5000 # Max number of iterations
+
+    iterations = 5000  # Max number of iterations
     x_points = [cur_x]
     i = 0
-    while (i < iterations):
+
+    while i < iterations:
         prev_x = cur_x
         cur_x -= rate * function_gradient(prev_x)
         if trunkate(prev_x) == trunkate(cur_x):
@@ -22,45 +25,47 @@ def find_minimum(function_gradient, cur_x, rate) -> list[float]:
         print(f'Iteration {i}')
         print(f'x = {cur_x:.2f}\n')
         x_points.append(cur_x)
-    
+
     print(f'Local min = {cur_x:.2f}')
     return x_points
 
 
 def find_minimum_vector(function_gradient, cur_x1, cur_x2, rate):
     print(f'Starting at x = ({cur_x1}, {cur_x2})\n')
-    precision = 0.001 # When to stop
-    prev_step_size = 1
-    iterations = 5000 # Max number of iterations
-    x_points = [(cur_x1, cur_x2)]
+
+    iterations = 5000  # Max number of iterations
+    x_y_points = [(cur_x1, cur_x2)]
     i = 0
-    while i < iterations and prev_step_size > precision:
+
+    while i < iterations:
         prev_x1, prev_x2 = cur_x1, cur_x2
         a, b = function_gradient(prev_x1, prev_x2)
         cur_x1 -= a*rate
         cur_x2 -= b*rate
-        prev_step_size = math.sqrt(math.pow(cur_x1-prev_x1, 2) + math.pow(cur_x2-prev_x2, 2))
+        if trunkate(prev_x1) == trunkate(cur_x1) and trunkate(prev_x2) == trunkate(cur_x2):
+            break
         i += 1
         print(f'Iteration {i}')
         print(f'x = ({cur_x1:.2f}, {cur_x2:.2f})\n')
-        x_points.append((cur_x1, cur_x2))
-    
+        x_y_points.append((cur_x1, cur_x2))
+
     print(f'Local min = ({cur_x1:.2f}, {cur_x2:.2f})')
+    return x_y_points
 
 
 if __name__ == '__main__':
     # Gradient of our function
-    function_g = lambda x: 2*x+20*math.pi*math.sin(2*math.pi*x) 
-    
+    function_g = lambda x: 2*x+20*math.pi*math.sin(2*math.pi*x)
+
     starting_x = -1/3
     learn_rate = 0.003
-    
+
     find_minimum(function_g, starting_x, learn_rate)
-    
-    #function_f = lambda x1, x2: [2*x1, 2*x2]
-    
-    #starting_x1 = 5
-    #starting_x2 = 14
-    #learn_rate = 0.005
-    
-    #find_minimum_vector(function_f, starting_x1, starting_x2, learn_rate)
+
+    function_f = lambda x1, x2: [2*x1, 2*x2]
+
+    starting_x1 = 5
+    starting_x2 = 14
+    learn_rate = 0.005
+
+    find_minimum_vector(function_f, starting_x1, starting_x2, learn_rate)
--- plots.py
+++ plots.py
@@ -1,10 +1,10 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
 
 def plot(x: list[float], x_points: list[float], function, save: bool = False):
-    # setting the axes at the centre
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.spines['left'].set_position('center')
@@ -14,16 +14,37 @@ def plot(x: list[float], x_points: list[float], function, save: bool = False):
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
 
-    # plot the function
     plt.plot(x, function, 'y', label='y=2^2+10-10cos(2πx)')
 
-    ax.plot(x_points[0], x_points[0]**2+10-10*np.cos(2*math.pi*x_points[0]), 'o-', color='red')
+    markersize = 4
+
+    ax.plot(x_points[0], x_points[0]**2+10-10*np.cos(2*math.pi*x_points[0]),
+            'o-', color='red', markersize=markersize)
     for x in x_points[1:]:
-        ax.plot(x, x**2+10-10*np.cos(2*math.pi*x), 'o-', color='grey')
+        ax.plot(x, x**2+10-10*np.cos(2*math.pi*x),
+                'o-', color='grey', markersize=markersize)
 
     plt.legend(loc='upper left')
 
-    # show the plot
     plt.show()
     if save:
         plt.savefig('plot')
+
+
+def plot_3d(x: list[float], y: list[float], x_y_points: list[tuple], function, save: bool = False):
+    ax = plt.axes(projection='3d')
+    ax.contour3D(x, y, function, 100, cmap='binary')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    markersize = 3
+
+    ax.plot(x_y_points[0][0], x_y_points[0][1], x_y_points[0][0]**2 +
+            x_y_points[0][1]**2, 'o-', color='red', markersize=markersize)
+    for x1, x2 in x_y_points[1:]:
+        ax.plot(x1, x2, x1**2+x2**2, 'o-', color='grey', markersize=markersize)
+
+    plt.show()
+    if save:
+        plt.savefig('plot_3d')
