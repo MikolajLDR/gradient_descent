1e21f72d7ad4b4019910f845963af6076e153fd7
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2021-10-27 11:48:31 +0200

    moved gradien descent to one functin

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,63 +9,50 @@ def trunkate(number: float) -> float:
     return int(number*10000)/10000
 
 
-def find_minimum(function_gradient, cur_x, rate) -> list[float]:
-    print(f'Starting at\nx = {cur_x:.2f}\n')
+def stop_condition(prev_xs: tuple, cur_xs: tuple) -> bool:
+    '''
+    Return False if any xs are equal with approximation
+    to 4 decimal places. Otherwise, return True.
+    '''
+    for prev_x, cur_x in zip(prev_xs, cur_xs):
+        if trunkate(prev_x) != trunkate(cur_x):
+            return False
+    return True
 
+
+def make_printable_xs(xs) -> str:
+    if len(xs) == 1:
+        return f'{xs[0]:.4f}'
+    else:
+        string = '('
+        for x in xs:
+            string += f'{x:.4f}, '
+        string = string[:-2] + ')'
+        return string
+
+
+def find_minimum(function_gradient, rate, *cur_xs) -> list:
+    '''
+    Find function's local minium with Gradient Descent algorithm.
+    Return list with all points.
+    '''
+    cur_xs = list(cur_xs)
+    print(f'Starting at\n{make_printable_xs(cur_xs)}')
+    
     iterations = 5000  # Max number of iterations
-    x_points = [cur_x]
+    x_points = [list(cur_xs)]
     i = 0
 
     while i < iterations:
-        prev_x = cur_x
-        cur_x -= rate * function_gradient(prev_x)
-        if trunkate(prev_x) == trunkate(cur_x):
+        prev_xs = list(cur_xs)
+        result = function_gradient(prev_xs)
+        for j, a in enumerate(result):
+            cur_xs[j] -= a*rate
+        if stop_condition(prev_xs, cur_xs):
             break
         i += 1
-        print(f'Iteration {i}')
-        print(f'x = {cur_x:.4f}\n')
-        x_points.append(cur_x)
+        print(f'Iteration {i}\n {make_printable_xs(cur_xs)}')
+        x_points.append(list(cur_xs))
 
-    print(f'Local min = {cur_x:.4f}')
+    print(f'Local min = {make_printable_xs(cur_xs)}')
     return x_points
-
-
-def find_minimum_vector(function_gradient, cur_x1, cur_x2, rate):
-    print(f'Starting at x = ({cur_x1}, {cur_x2})\n')
-
-    iterations = 5000  # Max number of iterations
-    x_y_points = [(cur_x1, cur_x2)]
-    i = 0
-
-    while i < iterations:
-        prev_x1, prev_x2 = cur_x1, cur_x2
-        a, b = function_gradient(prev_x1, prev_x2)
-        cur_x1 -= a*rate
-        cur_x2 -= b*rate
-        if trunkate(prev_x1) == trunkate(cur_x1) and trunkate(prev_x2) == trunkate(cur_x2):
-            break
-        i += 1
-        print(f'Iteration {i}')
-        print(f'x = ({cur_x1:.4f}, {cur_x2:.4f})\n')
-        x_y_points.append((cur_x1, cur_x2))
-
-    print(f'Local min = ({cur_x1:.4f}, {cur_x2:.4f})')
-    return x_y_points
-
-
-if __name__ == '__main__':
-    # Gradient of our function
-    function_g = lambda x: 2*x+20*math.pi*math.sin(2*math.pi*x)
-
-    starting_x = -1/3
-    learn_rate = 0.003
-
-    find_minimum(function_g, starting_x, learn_rate)
-
-    function_f = lambda x1, x2: [2*x1, 2*x2]
-
-    starting_x1 = 5
-    starting_x2 = 14
-    learn_rate = 0.005
-
-    find_minimum_vector(function_f, starting_x1, starting_x2, learn_rate)
--- main.py
+++ main.py
@@ -3,34 +3,34 @@ import math
 import numpy as np
 
 from plots import plot, plot_3d
-from gradient_descent import find_minimum, find_minimum_vector
+from gradient_descent import find_minimum
 
 if __name__ == '__main__':
     
     # ---------------- Function G ----------------
     
-    function_g = lambda x: 2*x+20*math.pi*math.sin(2*math.pi*x) 
+    function_g = lambda x: [2*x[0]+20*math.pi*math.sin(2*math.pi*x[0])]
 
-    starting_x = 0.5
-    learn_rate = 0.0025
+    starting_x = 0.4
+    learn_rate = 0.0004
 
-    x_points = find_minimum(function_g, starting_x, learn_rate)
-    plot_x = np.linspace(-0.6, 0.6, 200)
+    x_points = [x[0] for x in find_minimum(function_g, learn_rate, starting_x)]
+    plot_x = np.linspace(-0.1, 0.5, 200)
     function = plot_x**2+10-10*np.cos(2*math.pi*plot_x)
     
     plot(plot_x, x_points, function)
     
     # ---------------- Function F ----------------
     
-    function_f = lambda x1, x2: [2*x1, 2*x2]
+    function_f = lambda x: [2*x[0], 2*x[1]]
     
-    starting_x1 = 3
-    starting_x2 = 2
-    learn_rate = 0.2
+    starting_x1 = 300
+    starting_x2 = 200
+    learn_rate = 0.4
     
-    x_y_points = find_minimum_vector(function_f, starting_x1, starting_x2, learn_rate)
-    plot_x1 = np.linspace(-4, 4, 50)
-    plot_x2 = np.linspace(-4, 4, 50)
+    x_y_points = find_minimum(function_f, learn_rate, starting_x1, starting_x2)
+    plot_x1 = np.linspace(-310, 310, 50)
+    plot_x2 = np.linspace(-310, 310, 50)
     
     x, y = np.meshgrid(plot_x1, plot_x2)
     
